f7a7f0be91018c2c9d057ea7afab1d987781a945
IlCluchy <cluchybertolotti@gmail.com>
2026-04-22 08:21:54 +0200

    login.php

--- templates/header.php
+++ templates/header.php
@@ -3,12 +3,12 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <title>Progetto scuola</title>
+    <title>Torneo crazy</title>
     <link rel="stylesheet" href="./templates/style.css">
 </head>
 <body>
     <header>
         <div id="header_top">
-            <div>Registro ITIS</div>
+            <div>Torneo</div>
         </div>
     </header>
