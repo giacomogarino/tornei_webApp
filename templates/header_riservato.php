c5ab3882073035256ac90f7564bbfe5ae6ef911f
IlCluchy <cluchybertolotti@gmail.com>
2026-04-29 08:24:32 +0200

    update header_riservato.php

--- templates/header_riservato.php
+++ templates/header_riservato.php
@@ -6,37 +6,5 @@ if(!isset($_SESSION['login']))
     header("location: ./index.php?NecessariaAutentificazione");
 
 $current = basename($_SERVER['PHP_SELF']);
+require_once('header.php')
 ?>
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="../css/navbar.css">
-    <title>Torneo crazy</title>
-</head>
-<body>
-    <header>
-        <h1>WebApp Gestione Tornei</h1>
-        <h2>Ciao 
-            <?php if (isset($_SESSION['id_utente'])): ?>
-                <?= $_SESSION['nome_utente']; ?>
-            <?php else: ?>
-                <a href="login.php">Login</a>
-            <?php endif; ?>
-        </h2>
-        <div id="navbar">
-            <nav>
-                <a href="index.php" class="<?= $current === 'index.php' ? 'active' : '' ?>">Home</a>
-                <a href="profilo.php" class="<?= $current === 'profilo.php' ? 'active' : '' ?>">Profilo</a>
-                <a href="tornei_seguiti.php" class="<?= $current === 'tornei_seguiti.php' ? 'active' : '' ?>">Seguiti</a>
-                <a href="privati.php" class="<?= $current === 'privati.php' ? 'active' : '' ?>">Privati</a>
-
-                <?php if (!isset($_SESSION['id_utente'])): ?>
-                    <a href="login.php">Login</a>
-                <?php else: ?>
-                    <a href="logout.php">Logout</a>
-                <?php endif; ?>
-            </nav>
-        </div>
-    </header>
