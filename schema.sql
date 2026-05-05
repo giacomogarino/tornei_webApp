30d477ae14aabfc8683bae3b720a6208d8bf02a9
IlCluchy <cluchybertolotti@gmail.com>
2026-05-05 15:15:23 +0200

    controllo giocatori

--- schema.sql
+++ schema.sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Host: localhost:3306
--- Creato il: Apr 28, 2026 alle 14:19
+-- Creato il: Mag 05, 2026 alle 15:04
 -- Versione del server: 10.11.16-MariaDB-cll-lve-log
 -- Versione PHP: 8.4.20
 
@@ -20,8 +20,6 @@ SET time_zone = "+00:00";
 --
 -- Database: `itpbrgro_wp761`
 --
-CREATE DATABASE IF NOT EXISTS `itpbrgro_wp761` DEFAULT CHARACTER SET utf8mb3 COLLATE utf8mb3_general_ci;
-USE `itpbrgro_wp761`;
 
 -- --------------------------------------------------------
 
@@ -54,6 +52,13 @@ CREATE TABLE `giocatore_squadra` (
   `utente_id` int(11) NOT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
+--
+-- Dump dei dati per la tabella `giocatore_squadra`
+--
+
+INSERT INTO `giocatore_squadra` (`id`, `squadra_id`, `utente_id`) VALUES
+(52, 12, 22);
+
 -- --------------------------------------------------------
 
 --
@@ -65,8 +70,8 @@ CREATE TABLE `partita` (
   `torneo_id` int(11) NOT NULL,
   `squadra_casa_id` int(11) NOT NULL,
   `squadra_ospite_id` int(11) NOT NULL,
-  `gol_casa` int(11) DEFAULT NULL,
-  `gol_ospite` int(11) DEFAULT NULL,
+  `punti_casa` int(11) DEFAULT NULL,
+  `punti_ospite` int(11) DEFAULT NULL,
   `girone` int(11) DEFAULT NULL,
   `turno` enum('ottavi','quarti','semifinale','finale') DEFAULT NULL,
   `giocata_il` timestamp NULL DEFAULT NULL,
@@ -84,8 +89,17 @@ CREATE TABLE `squadra` (
   `torneo_id` int(11) NOT NULL,
   `nome` varchar(100) NOT NULL,
   `capitano_id` int(11) NOT NULL,
-  `stato` enum('in_attesa','approvata','rifiutata') NOT NULL DEFAULT 'in_attesa'
+  `stato` enum('in_attesa','approvata','rifiutata') NOT NULL DEFAULT 'in_attesa',
+  `token_approva` varchar(64) DEFAULT NULL,
+  `token_rifiuta` varchar(64) DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
+
+--
+-- Dump dei dati per la tabella `squadra`
+--
+
+INSERT INTO `squadra` (`id`, `torneo_id`, `nome`, `capitano_id`, `stato`, `token_approva`, `token_rifiuta`) VALUES
+(12, 27, 'squadra1', 22, 'in_attesa', '17338fcfb4caaf78afe60bbfd873043fe5aeb8569f91249492b413a0242207a8', 'c00956207d3c10ea05c2de9f7e124191f01db03378e3794827ff4511a05bc6fc');
 
 -- --------------------------------------------------------
 
@@ -95,8 +109,10 @@ CREATE TABLE `squadra` (
 
 CREATE TABLE `torneo` (
   `id` int(11) NOT NULL,
+  `sport` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `nome` varchar(150) NOT NULL,
   `descrizione` varchar(255) DEFAULT NULL,
+  `luogo` varchar(255) CHARACTER SET utf8mb4 COLLATE utf8mb4_unicode_ci NOT NULL,
   `formato` enum('girone_unico','eliminazione_diretta','gironi_playoff') NOT NULL,
   `tipo_partita` enum('andata','andata_ritorno') NOT NULL,
   `visibilita` enum('pubblico','privato') NOT NULL DEFAULT 'pubblico',
@@ -114,9 +130,10 @@ CREATE TABLE `torneo` (
 -- Dump dei dati per la tabella `torneo`
 --
 
-INSERT INTO `torneo` (`id`, `nome`, `descrizione`, `formato`, `tipo_partita`, `visibilita`, `numero_squadre`, `creato_da`, `stato`, `min_giocatori_per_squadra`, `max_giocatori_per_squadra`, `min_squadre`, `data_chiusura_iscrizioni`, `codice_privato`) VALUES
-(2, 'prova', 'prova', 'girone_unico', 'andata', 'pubblico', 8, 22, 'aperto', 5, 10, 8, '2026-05-27 17:41:00', NULL),
-(3, 'prova1', 'prova1', 'gironi_playoff', 'andata_ritorno', 'privato', 8, 22, 'aperto', 5, 10, 8, '2026-05-27 17:41:00', 'EFADB86B');
+INSERT INTO `torneo` (`id`, `sport`, `nome`, `descrizione`, `luogo`, `formato`, `tipo_partita`, `visibilita`, `numero_squadre`, `creato_da`, `stato`, `min_giocatori_per_squadra`, `max_giocatori_per_squadra`, `min_squadre`, `data_chiusura_iscrizioni`, `codice_privato`) VALUES
+(25, 'calcio', 'Kings league', 'Copia di Blur', 'Cuneo', 'eliminazione_diretta', 'andata', 'pubblico', 8, 25, 'aperto', 1, 5, 4, '2032-10-05 14:55:00', NULL),
+(26, 'calcio', 'Youtuber league', 'Copia di PirlasV', 'Milano', 'girone_unico', 'andata', 'pubblico', 8, 25, 'aperto', 1, 5, 4, '2032-10-05 14:55:00', NULL),
+(27, 'calcio', 'Goat league', 'Creato da Liam', 'Valdieri', 'gironi_playoff', 'andata', 'pubblico', 8, 25, 'aperto', 1, 5, 4, '2032-10-26 14:55:00', NULL);
 
 -- --------------------------------------------------------
 
@@ -145,17 +162,26 @@ CREATE TABLE `utente` (
   `cod_ci` varchar(20) NOT NULL,
   `verified` tinyint(1) NOT NULL DEFAULT 0,
   `token` varchar(64) DEFAULT NULL,
-  `created_at` timestamp NOT NULL DEFAULT current_timestamp()
+  `created_at` timestamp NOT NULL DEFAULT current_timestamp(),
+  `token_expiry` datetime DEFAULT NULL
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8mb4 COLLATE=utf8mb4_unicode_ci;
 
 --
 -- Dump dei dati per la tabella `utente`
 --
 
-INSERT INTO `utente` (`id`, `nome`, `cognome`, `email`, `password`, `cod_ci`, `verified`, `token`, `created_at`) VALUES
-(22, 'Luca', 'Bertolotti', 'cluchybertolotti@gmail.com', 'chvQ7Ow41U4RU', 'CA09365PZ', 1, NULL, '2026-04-25 10:49:26'),
-(24, 'Liam', 'Tu', 'tusailiam@gmail.com', 'chvQ7Ow41U4RU', 'CA1f23e45', 1, NULL, '2026-04-27 10:00:43'),
-(25, 'Matteo', 'Luciano', 'matteo.luciano07@gmail.com', 'chvQ7Ow41U4RU', 'CA840', 1, NULL, '2026-04-27 10:03:36');
+INSERT INTO `utente` (`id`, `nome`, `cognome`, `email`, `password`, `cod_ci`, `verified`, `token`, `created_at`, `token_expiry`) VALUES
+(22, 'Luca', 'Bertolotti', 'cluchybertolotti@gmail.com', 'chvQ7Ow41U4RU', 'CA09365PZ', 1, NULL, '2026-04-25 10:49:26', NULL),
+(24, 'Liam', 'Tu', 'tusailiam@gmail.com', 'chvQ7Ow41U4RU', 'CA1f23e45', 1, NULL, '2026-04-27 10:00:43', NULL),
+(25, 'Matteo', 'Luciano', 'matteo.luciano07@gmail.com', 'chvQ7Ow41U4RU', 'CA840', 1, NULL, '2026-04-27 10:03:36', NULL),
+(26, 'Giacomo', 'Garino', 'giacomo.garino@itiscuneo.edu.it', 'chvQ7Ow41U4RU', 'CA66868KR', 1, NULL, '2026-04-28 12:24:34', NULL),
+(27, 'Martina', 'Luciano', 'martina.luciano2009@gmail.com', 'chvQ7Ow41U4RU', 'CA86945RB', 1, NULL, '2026-04-30 05:13:04', NULL),
+(28, 'Matteo', 'Olivero', 'olivero.matteo007@gmail.com', 'chvQ7Ow41U4RU', 'Aa1123bb', 1, NULL, '2026-04-30 11:55:50', NULL),
+(29, 'Sai', 'You', 'sailiam.tu@itiscuneo.edu.it', 'chHGEgcEngOcc', 'CA09365jstnf', 1, NULL, '2026-05-05 12:12:30', NULL),
+(30, 'Filippo', 'Gondolo', 'filippo.gondolo@gmail.com', 'cha/.4kD3E0Ek', 'CAry87t9y09', 1, NULL, '2026-05-05 12:15:50', NULL),
+(31, 'Francesco', 'Torterolo', 'torterolofrancesco@gmail.com', 'ch69ffFeBBhXM', 'CAei3mc0r2h9', 1, NULL, '2026-05-05 12:18:57', NULL),
+(32, 'Torta', 'France', 'francesco.torterolo@itiscuneo.edu.it', 'chzEBL2/VXci.', 'CAhncr23902', 1, NULL, '2026-05-05 12:21:59', NULL),
+(33, 'Gondy', 'Pippo', 'filippo.gondolo@itiscuneo.edu.it', 'chdxK5wD9Rt8I', 'CAnceh39h0', 1, NULL, '2026-05-05 12:23:24', NULL);
 
 --
 -- Indici per le tabelle scaricate
@@ -231,7 +257,7 @@ ALTER TABLE `classifica`
 -- AUTO_INCREMENT per la tabella `giocatore_squadra`
 --
 ALTER TABLE `giocatore_squadra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=55;
 
 --
 -- AUTO_INCREMENT per la tabella `partita`
@@ -243,25 +269,25 @@ ALTER TABLE `partita`
 -- AUTO_INCREMENT per la tabella `squadra`
 --
 ALTER TABLE `squadra`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=14;
 
 --
 -- AUTO_INCREMENT per la tabella `torneo`
 --
 ALTER TABLE `torneo`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=28;
 
 --
 -- AUTO_INCREMENT per la tabella `torneo_seguito`
 --
 ALTER TABLE `torneo_seguito`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=18;
 
 --
 -- AUTO_INCREMENT per la tabella `utente`
 --
 ALTER TABLE `utente`
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=26;
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=34;
 
 --
 -- Limiti per le tabelle scaricate
