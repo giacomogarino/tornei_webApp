7ec6093eeca82ece1d63711ced09606dca8ec08a
Matteo Luciano <matteo.luciano07@gmail.com>
2026-05-06 08:37:03 +0200

    aggiunta visione squadre nei tornei

---
+++ components/squadre_torneo.php
@@ -0,0 +1,36 @@
+<?php
+
+/**
+ * Mostra le squadre approvate passate come array.
+ *
+ * @param array    $squadre    Risultato della query (array di associativi)
+ * @param int|null $utente_id  ID utente loggato (opzionale, per label "sei il capitano")
+ */
+function mostra_squadre_approvate(array $squadre, ?int $utente_id = null): void
+{
+    if (empty($squadre)): ?>
+        <p><em>Nessuna squadra approvata al momento.</em></p>
+    <?php else: ?>
+        <table border="1" cellpadding="8" cellspacing="0" width="100%">
+            <tr>
+                <th align="left">#</th>
+                <th align="left">Nome squadra</th>
+                <th align="left"></th>
+            </tr>
+            <?php foreach ($squadre as $i => $squadra): ?>
+            <tr>
+                <td><?= $i + 1 ?></td>
+                <td>
+                    <?= htmlspecialchars($squadra['nome']) ?>
+                    <?php if ($utente_id && $squadra['capitano_id'] == $utente_id): ?>
+                        <em>(sei il capitano)</em>
+                    <?php endif; ?>
+                </td>
+                <td>
+                    <a href="../dettagli_squadra.php?id=<?= $squadra['id'] ?>">Vedi squadra</a>
+                </td>
+            </tr>
+            <?php endforeach; ?>
+        </table>
+    <?php endif;
+}
